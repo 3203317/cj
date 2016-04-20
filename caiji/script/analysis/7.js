335698a9a89b042d17f8e0ec0033ebf30c427b7c
unknown <3203317@qq.com>
2016-04-20 14:39:44 +0800

    haha

--- caiji/script/analysis/7.js
+++ caiji/script/analysis/7.js
@@ -5,7 +5,38 @@
  */
 result = {
 	success: true,
-	data: docs
+	data: []
 }
+
+function analysis(doc){
+	var $ = cheerio.load(doc.html, { decodeEntities: false });
+
+	// 标题
+	var title = $('#film').find('.detail_pic1').find('>img').attr('alt');
+
+	// 图片
+	var img = $('#film').find('.detail_pic1').find('>img').attr('src');
+
+	var newInfo = {
+		IMG: img,
+		TITLE: title
+	};
+
+	return newInfo;
+}
+
+(function(){
+	for(var i in docs){
+		var doc = docs[i];
+
+		// TODO
+		if((2 === doc.DEPTH) && !!doc.html){
+			// 分析数据
+			var newInfo = analysis(doc);
+
+			result.data.push(newInfo);
+		} // IF
+	} // FOR
+})();
 
 html = null;
